b8355c227e3a88fdb6649c1caf0736320bc15e8c
Mahdis <mahdis.masoumi.sh@gmail.com>
2023-08-09 16:00:01 -0600

    updating posts

---
+++ src/components/Post.js
@@ -0,0 +1,12 @@
+import styles from "./Post.module.css";
+function Post({ author, body }) {
+  //   const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+  return (
+    <li className={styles.post}>
+      <p className={styles.author}>{author}</p>
+      <p className={styles.text}>{body}</p>
+    </li>
+  );
+}
+
+export default Post;
